0f3a8352c6afe76f59defcfd5b153d2c5c36b681
Laurence Cortez <laurencecortez26.lc@gmail.com>
2024-08-18 20:05:54 +0800

    global filtering

---
+++ react-tables/src/components/FilteringTable.js
@@ -0,0 +1,87 @@
+
+import { useMemo } from 'react';
+import { useTable, useGlobalFilter } from 'react-table';
+import { MOCK_DATA } from '../MOCK_DATA';
+import { GROUP_COLUMNS } from './columns';
+import { GlobalFilter } from './GlobalFilter';
+
+export const FilteringTable = () => {
+  const columns = useMemo(() => GROUP_COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+  const {
+    getTableProps,
+    headerGroups,
+    getTableBodyProps,
+    rows,
+    prepareRow,
+    footerGroups,
+    state: { globalFilter },
+    setGlobalFilter
+  } = useTable({
+    columns,
+    data,
+  }, useGlobalFilter)
+
+  return (
+    <>
+      <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+      <table {...getTableProps}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            // Apply the header row props
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {// Loop over the headers in each row
+                headerGroup.headers.map(column => (
+                  // Apply the header cell props
+                  <th {...column.getHeaderProps()}>
+                    {// Render the header
+                      column.render('Header')}
+                  </th>
+                ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {// Loop over the table rows
+            rows.map(row => {
+              // Prepare the row for display
+              prepareRow(row)
+              return (
+                // Apply the row props
+                <tr {...row.getRowProps()}>
+                  {// Loop over the rows cells
+                    row.cells.map(cell => {
+                      // Apply the cell props
+                      return (
+                        <td {...cell.getCellProps()}>
+                          {// Render the cell contents
+                            cell.render('Cell')}
+                        </td>
+                      )
+                    })}
+                </tr>
+              )
+            })}
+        </tbody>
+        <tfoot>
+          {
+            footerGroups.map(footerGroup => (
+              <tr {...footerGroup.getFooterGroupProps()}>
+                {
+                  footerGroup.headers.map(column => (
+                    <th {...column.getFooterProps()}>
+                      {
+                        column.render('Footer')
+                      }
+                    </th>
+                  ))
+                }
+              </tr>
+            ))
+          }
+        </tfoot>
+      </table>
+    </>
+
+  )
+}
