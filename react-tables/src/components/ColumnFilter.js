b00fac6dcc30067650c5e1ba1eb77a8a74ea22b8
Laurence Cortez <laurencecortez26.lc@gmail.com>
2024-08-19 21:10:34 +0800

    filter debounce

--- react-tables/src/components/ColumnFilter.js
+++ react-tables/src/components/ColumnFilter.js
@@ -1,10 +1,20 @@
+import { useState } from 'react';
+import { useAsyncDebounce } from 'react-table';
 
 export const ColumnFilter = ({ column }) => {
   const { filterValue, setFilter } = column;
+  const [value, setValue] = useState(filterValue);
+  const onChange = useAsyncDebounce((value) => {
+    setFilter(value || undefined);
+  }, 300);
+
   return (
     <span>
       Search: {' '}
-      <input value={filterValue || ''} onChange={e => setFilter(e.target.value)} />
+      <input value={value || ''} onChange={e => {
+        setValue(e.target.value)
+        onChange(e.target.value)
+      }} />
     </span>
   )
 }
