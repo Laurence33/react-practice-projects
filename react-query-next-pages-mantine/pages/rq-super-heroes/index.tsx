62ee1a74fc34a3bf0ab4b6b175547c806533e46f
Laurence Cortez <laurencecortez26.lc@gmail.com>
2024-08-25 16:45:31 +0800

    custom cache timer

--- react-query-next-pages-mantine/pages/rq-super-heroes/index.tsx
+++ react-query-next-pages-mantine/pages/rq-super-heroes/index.tsx
@@ -3,7 +3,13 @@ import { useQuery } from 'react-query';
 
 const fetchSuperHeroes = () => axios.get('http://localhost:4000/superheroes');
 function index() {
-  const { isLoading, data, isError, error } = useQuery('super-heroes', fetchSuperHeroes);
+  const { isLoading, isFetching, data, isError, error } = useQuery(
+    'super-heroes',
+    fetchSuperHeroes,
+    {
+      cacheTime: 5000, // in milliseconds
+    }
+  );
 
   if (isLoading) return <h2>Loading...</h2>;
 
