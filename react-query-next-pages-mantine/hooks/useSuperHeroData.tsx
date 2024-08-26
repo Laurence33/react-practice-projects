37367649289614d5d839f833809815e4bf67b393
Laurence Cortez <laurencecortez26.lc@gmail.com>
2024-08-26 13:50:10 +0800

    short-hand using queryKey

--- react-query-next-pages-mantine/hooks/useSuperHeroData.tsx
+++ react-query-next-pages-mantine/hooks/useSuperHeroData.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from 'react-query';
 import axios from 'axios';
 
-const fetcher = (heroId: string) => axios.get(`http://localhost:4000/superheroes/${heroId}`);
-export const useSuperHeroData = (heroId: string) =>
-  useQuery(['super-hero', heroId], () => fetcher(heroId));
+const fetcher = ({ queryKey }) => {
+  const heroId = queryKey[1];
+  return axios.get(`http://localhost:4000/superheroes/${heroId}`);
+};
+export const useSuperHeroData = (heroId: string) => useQuery(['super-hero', heroId], fetcher);
