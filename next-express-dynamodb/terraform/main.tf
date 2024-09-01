43e4eedca374474fd76be372eb0cc3e4d7206a4d
Laurence Cortez <laurencecortez26.lc@gmail.com>
2024-09-01 09:24:04 +0800

    replace users table

--- next-express-dynamodb/terraform/main.tf
+++ next-express-dynamodb/terraform/main.tf
@@ -6,15 +6,9 @@ resource "aws_dynamodb_table" "users" {
   name         = "users"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "id"
-  range_key    = "createdAt"
 
   attribute {
     name = "id"
-    type = "S"
-  }
-
-  attribute {
-    name = "createdAt"
     type = "S"
   }
 
