1b6b91b8797d44f70864804a5f8a41b23516cb1e
Laurence Cortez <laurencecortez26.lc@gmail.com>
2024-09-01 08:48:12 +0800

    terraform setup

---
+++ next-express-dynamodb/terraform/main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_dynamodb_table" "users" {
+  name         = "users"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+  range_key    = "createdAt"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "createdAt"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Next-Express-DynamoDB"
+  }
+}
